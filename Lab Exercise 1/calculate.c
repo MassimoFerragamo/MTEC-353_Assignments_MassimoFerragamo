e719ce83f3ce663aa4cc6cdbf85eb66090f6161d
MassimoFerragamo <maxfer1810@gmail.com>
2026-05-26 20:01:10 -0400

    class work

---
+++ Lab Exercise 1/calculate.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float a = 12.5;
+    float b = 8.3;
+    float out =  a * b;
+    printf("The area of the rectangle is: %.2f\n", out);
+}
--- Lab Exercise 1/helloWorld.c
+++ Lab Exercise 1/helloWorld.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello, World\n");
+    printf("Hello\nWorld\nWorld\nWorld\n");
 }
+
+//syntax
+/* 
+all functions start with the output type
+int main(){} returns an integer
+
+main function does not need return statement
+
+in the command line compile and then run 
+
+for example compile: 
+clang HelloWorld.c 
+
+outputs a.out:
+run the code 
+./a.out
+
+
+name your executable Hello World
+
+-lm include math library 
+
+
+learned about how I can define blank variables in c and then assign values to them later on in the code.
+*/
