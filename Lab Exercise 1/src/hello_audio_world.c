3cf638dd470379dc94db434274779afb5747afda
MassimoFerragamo <maxfer1810@gmail.com>
2026-06-08 23:28:42 -0400

    now all of my values are in an array, I was also thinking about other methods that I might implement later

--- Lab Exercise 1/src/hello_audio_world.c
+++ Lab Exercise 1/src/hello_audio_world.c
@@ -3,13 +3,16 @@
 
 int main() {
     int count = 40; 
-    double arr[count]; 
+    int arr[count]; // I might not even need this actually, I can just iterrate over 10 lines?
+    // I am gonna try with the array first and then try without 
+    // maybe array is the most inexpensive because I calculate less
     double amplitude = 10;
     int phase;
     float frequency = 0.5; 
 
     for (int i = 0; i < count; i++){
         phase = round(sin(i * frequency) * amplitude);
-        printf("%d \n", phase);
+        arr[i] = phase;
     }
+    
 }
