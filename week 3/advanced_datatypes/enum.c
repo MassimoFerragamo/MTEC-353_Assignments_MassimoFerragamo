b787f2e0d0cacc0aa96bbca3916e965298b4ee47
MassimoFerragamo <maxfer1810@gmail.com>
2026-06-16 13:53:16 -0400

    review

--- week 3/advanced_datatypes/enum.c
+++ week 3/advanced_datatypes/enum.c
@@ -4,13 +4,16 @@
 //enum between keyword
 //helps with readability 
 
-enum Waveform {
-    sine, square, sawtooth
-};
+// enum is useful because it lets you define a set of named integer constants 
+
+typedef enum Waveform {
+    sine, square, sawtooth, triangle, wt1 = 8, wt2, wt3 = 50, wt4, // you can define a number in here and then enum will continue counting up so wt4 is 51
+} Waveform;
+
 
 //enum then enum name, inside function then keywords inside
 int main(){
-    enum Waveform wf = sine; //messy syntax, use typedef to rename this
+    Waveform wf = wt4; //messy syntax, use typedef to rename this
     // same thing as variable declaration
     printf("%d\n", wf);
 }
