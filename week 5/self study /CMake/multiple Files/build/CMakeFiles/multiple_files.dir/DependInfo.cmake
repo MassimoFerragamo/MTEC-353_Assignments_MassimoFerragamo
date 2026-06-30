1b7cf38ec3f11c5056d8a349576cbf0d6630df0f
MassimoFerragamo <maxfer1810@gmail.com>
2026-06-30 11:06:14 -1000

    cmake

---
+++ week 5/self study /CMake/multiple Files/build/CMakeFiles/multiple_files.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/massimo/Documents/GitHub/MTEC-353_Assignments_MassimoFerragamo/week 5/self study /CMake/multiple Files/src/main.cpp" "CMakeFiles/multiple_files.dir/src/main.cpp.o" "gcc" "CMakeFiles/multiple_files.dir/src/main.cpp.o.d"
+  "/Users/massimo/Documents/GitHub/MTEC-353_Assignments_MassimoFerragamo/week 5/self study /CMake/multiple Files/src/module.cpp" "CMakeFiles/multiple_files.dir/src/module.cpp.o" "gcc" "CMakeFiles/multiple_files.dir/src/module.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
