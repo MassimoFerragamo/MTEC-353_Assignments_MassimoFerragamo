77bcd9f014a402b15d480256501fbd3ee56e7594
MassimoFerragamo <maxfer1810@gmail.com>
2026-06-30 15:42:31 -1000

    trying again from scratch

--- week 5/self study /JUCE/My Project/ConsoleApp/Main.cpp
+++ week 5/self study /JUCE/My Project/ConsoleApp/Main.cpp
@@ -1,10 +1,11 @@
 #include <juce_core/juce_core.h>
+#include <iostream>
 
 int main (int argc, char* argv[])
 {
-
     // Your code goes here!
     juce::ignoreUnused (argc, argv);
+    std::cout << "YESSSS" << std::endl;
 
     return 0;
 }
